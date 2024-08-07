3ff3098ef56858c1bc86ccba8ea506a9d3e0a267
spidercupcake <xmze11690911300@gmail.com>
2024-08-07 21:00:50 +0500

    Refactor Graph class and update main function

--- graphs.cpp
+++ graphs.cpp
@@ -3,20 +3,31 @@ using namespace std;
 
 class Graph {
     private:
-        int id;
-        int name;
+        int firstElement;
+        int secondElement;
 
     public:
         // Constructor
-        Graph(int id, int name) {
-            this->id = id;
-            this->name = name;
+        Graph(int firstElement, int secondElement) {
+            this->firstElement = firstElement;
+            this->secondElement = secondElement;
             cout << "Graph constructor executed." << endl;
+            cout << "First elemet.	" << firstElement << endl;
+            cout << "Second elemet.	" << secondElement << endl;
         }
 };
 
+
+
 int main() {
     // Passing arguments to the constructor
-    Graph graph1(1, 10);
+    int adjacencymatrix[5][5] = {
+    	{1,2,3,4,5},
+    	{6,7,8,9,0},
+    	{1,2,3,4,5},
+    	{6,7,8,9,0},
+    	{1,2,3,4,5},
+	};
+    Graph graph1(adjacencymatrix[1][1], adjacencymatrix[2][2]);
     return 0;
 }
